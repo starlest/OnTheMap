18232fafe479174dd77293e05c13fbf423c63d7b
Edwin Chia <edwinchia@Edwins-MacBook-Pro.local>
2016-06-02 18:07:54 +0700

    Fix loginScreen layout constrains.

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var udacityButtonLoginButton: UIButton!
+    @IBOutlet weak var facebookLoginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func udacityLoginButtonPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func facebookLoginButtonPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func signupButtonPressed(sender: AnyObject) {
+    }
+    
+    private func setUIEnabled(enabled: Bool) {
+        
     }
 }
 
