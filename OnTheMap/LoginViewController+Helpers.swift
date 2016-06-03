bc400cfcc943d5a3084c039506d57bb6f9908c59
Edwin Chia <edwinchia@Edwins-MacBook-Pro.local>
2016-06-03 07:47:53 +0700

    Fix LoginViewController inability to access private helper methods in extension file

--- OnTheMap/LoginViewController+Helpers.swift
+++ OnTheMap/LoginViewController+Helpers.swift
@@ -10,14 +10,14 @@ import Foundation
 
 extension LoginViewController {
     
-    private func areLoginFieldsFilled() -> Bool {
+    func areLoginFieldsFilled() -> Bool {
         if emailTextField.text == nil || passwordTextField.text == nil {
             return false
         }
         return emailTextField.text?.characters.count > 0 && passwordTextField.text?.characters.count > 0
     }
     
-    private func showAlert(title: String, message: String) {
+    func showAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
         let alertAction = UIAlertAction(title: "OK", style: .Default, handler: { (UIAlertAction) in
             self.dismissViewControllerAnimated(true, completion: nil)
